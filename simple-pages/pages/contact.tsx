657803349d95e74328532c0755ec647e38091ad1
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-06-30 00:42:48 +0500

    Routing completed
    Routing completed

---
+++ simple-pages/pages/contact.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const contact: NextPage = () => {
+	return (
+		<div>
+			<h1>Contact Page</h1>
+			<p>Name: Muhammad Mishaal khan</p>
+			<p>Email: khan.mishaal91@gmail.com</p>
+			<p>Contact.no: 03363650852</p>
+		</div>
+	);
+};
+
+export default contact;
